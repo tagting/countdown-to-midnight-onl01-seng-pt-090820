4da05319e0cb09efecd94b88f66d13880c59d840
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-22 08:55:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  while number != 0
+    puts "#{number} SECOND(S)!"
+      number -= 1
+  if number == 0
+    puts "Hey"
+    end
+  end
 end
+
+  
+
+
