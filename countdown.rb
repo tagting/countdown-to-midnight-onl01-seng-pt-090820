b5d5c2fea8dcbf78d39200a169f2dab196d38364
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-22 09:16:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@ def countdown(number)
   while number != 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    break if number == 0
+    break if number <= 0
   end
 print "Happy New Year!".upcase
 end
