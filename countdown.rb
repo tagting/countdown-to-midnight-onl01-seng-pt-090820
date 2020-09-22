764eb098bff1cc5d56080fc80fb78cee66e173f2
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-22 08:57:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 def countdown(number)
   while number != 0
-    return "#{number} SECOND(S)!"
-      number -= 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
   if number == 0
     puts "HAPPY NEW YEAR!"
     end
